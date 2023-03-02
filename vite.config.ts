f951cc4580cda63fdb29e6eff1abbbe8735a0553
Mykhailo <dmakgregor@gmail.com>
2023-03-02 18:21:56 +0200

    sp

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://github.com/dmakgregor/fcc-drum-pad'
 });
